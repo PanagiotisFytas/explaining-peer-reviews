edc733cf843999f0cd70f688b6371d79fed8b756
Panagiotis <pfytas@durga.shef.ac.uk>
2020-07-06 17:32:45 +0100

    add some stuff from causalBert (e.g. ATE-unadjusted

--- src/combine_explanations.py
+++ src/combine_explanations.py
@@ -1,11 +1,19 @@
 import pickle
 import pandas as pd
 from DataLoader import DataLoader
+import os
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+from helper_functions import natural_sort_key
+import re
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
+
+lemmatizer = WordNetLemmatizer()
 
 cols = ['Words', 'Mean', '#Total', '#Positive', '#Neg', 'PosMean', 'NegMean']
-# final_decision = 'only'
-final_decision = 'exclude'
+final_decision = 'only'
+# final_decision = 'exclude'
 if final_decision == 'only':
     clf_to_explain = 'final_decision_only'
 else:
@@ -47,14 +55,76 @@ class WordMetrics:
                             columns=cols)
 
 
-if __name__ == '__main__':
-    indexes = range(0, 42)
+def combine_explanation_in_matrix(binary=False):
+    explanation_files = [os.path.join(path, file) for file in os.listdir(path) if file.endswith('.p') and 'explanation' in file]
+    explanation_files.sort(key=natural_sort_key)
+    explanations = []
+    word_to_idx = {}
+    idx_to_word = []
+    idx = 0
+    # create word_to_idx and idx_to_word
+    for f in explanation_files:
+        exp = pickle.load(open(f, 'rb'))
+        exp = exp.as_list()
+        explanations.append(exp)
+        for word, _ in exp:
+            word = lemmatizer.lemmatize(word.lower())
+            if word not in word_to_idx:
+                idx_to_word.append(word)
+                word_to_idx[word] = idx
+                idx += 1
+    
+    words_idx_tuple = sorted(word_to_idx.items(), key=lambda x: x[1]) # sort words by idx in increasing order
+    words = [item[0] for item in words_idx_tuple] # get only the words
+
+    samples = len(explanations)
+    features = len(word_to_idx)
+    exp_bow = np.zeros((samples, features))
+    for i, exp in enumerate(explanations):
+        for word, importance in exp:
+            word = lemmatizer.lemmatize(word.lower())
+            if binary:
+                exp_bow[i][word_to_idx[word]] = 1
+            else:
+                exp_bow[i][word_to_idx[word]] = importance
+
+    return exp_bow, words
+
+
+def get_explanation_labels():
+    explanation_files = [os.path.join(path, file) for file in os.listdir(path) if file.endswith('.txt') and 'explanation' in file]
+    explanation_files.sort(key=natural_sort_key)
+    predicted_labels = []
+    actual_labels = []
+    for f in explanation_files:
+        f = open(f, 'r')
+        lines = [line for line in f.readlines()]
+        f.close()
+        prediction = float(re.findall("\d+\.\d+", lines[1])[0]) # read the predicted value (output of the model)
+        if prediction >= 0.5:
+            predicted_labels.append(1)
+        else:
+            predicted_labels.append(0)
+        if 'accepted' in lines[3]:
+            actual_labels.append(1)
+        elif 'rejected' in lines[3]:
+            actual_labels.append(0)
+    return np.array(predicted_labels), np.array(actual_labels)
+    
+
+def combine_explanations(clf_to_explain=clf_to_explain, criterion='abs'):
+    """
+    :param criterion: 'abs' | 'pos' | 'neg'
+    """
+    path = DataLoader.DATA_ROOT / clf_to_explain
+    explanation_files = [os.path.join(path, file) for file in os.listdir(path) if file.endswith('.p') and 'explanation' in file]
+    explanation_files.sort(key=natural_sort_key)
 
     # combine all explanations
     word_metrics_acc = {}
 
-    for idx in indexes:
-        exp = pickle.load(open(path / ('explanation' + str(idx) + '.p'), 'rb'))
+    for idx, f in enumerate(explanation_files):
+        exp = pickle.load(open(f, 'rb'))
         exp_list = exp.as_list()
         for word, importance in exp_list:
             if word.lower() in word_metrics_acc:
@@ -69,10 +139,19 @@ if __name__ == '__main__':
     for metric in word_metrics_acc.values():
         combined_words = combined_words.append(metric.to_df(),  ignore_index=True)
 
-
-    # combined_words = combined_words.sort_values(['#Total', 'Mean'], ascending=[False, False])
-    abs_df = combined_words.copy()
-    abs_df['Mean'] = abs_df['Mean'].abs()
-    combined_words = combined_words.reindex(abs_df.sort_values('Mean', ascending=False).index)
+    if criterion == 'abs':
+        # combined_words = combined_words.sort_values(['#Total', 'Mean'], ascending=[False, False])
+        abs_df = combined_words.copy()
+        abs_df['Mean'] = abs_df['Mean'].abs()
+        combined_words = combined_words.reindex(abs_df.sort_values('Mean', ascending=False).index)
+    elif criterion == 'pos':
+        pos_df = combined_words.copy()
+        combined_words = combined_words.reindex(pos_df.sort_values('Mean', ascending=False).index)
+    elif criterion == 'neg':
+        # combined_words = combined_words.sort_values(['#Total', 'Mean'], ascending=[False, False])
+        neg_df = combined_words.copy()
+        combined_words = combined_words.reindex(neg_df.sort_values('Mean', ascending=True).index)
+    
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         print(combined_words)
+    return combined_words
--- src/models.py
+++ src/models.py
@@ -172,3 +172,76 @@ class ScoreClassifier(nn.Module):
         out = self.last_fc(out)
         out = torch.sigmoid(out)
         return out
+
+
+class CausalBERT(nn.Module):
+    """Fine tune Bert with for causal inference
+    """
+    DATA_ROOT = pathlib.Path(os.environ['DATA'])
+    SCIBERT_PATH = str(DATA_ROOT / 'scibert_scivocab_uncased')
+    def __init__(self, freeze_bert=False):
+        """
+        @param    bert: a BertModel object
+        @param    classifier: a torch.nn.Module classifier
+        @param    freeze_bert (bool): Set `False` to fine-tune the BERT model
+        """
+        super(BertClassifier, self).__init__()
+        # Specify hidden size of BERT, hidden size of our classifier, and number of labels
+        D_in, H, D_out = 768, 200, 2
+
+        # Instantiate BERT model
+        self.bert = BertModel.from_pretrained(self.SCIBERT_PATH)
+
+        # Instantiate an one-layer feed-forward classifier
+        self.propensity = nn.Sequential(
+            nn.Linear(D_in, 1)
+        )
+
+        self.expectations == nn.ModuleList([
+            nn.Sequential( # for treatment = 0
+                nn.Linear(D_in, H),
+                nn.ELU(),
+                nn.Linear(H, 1)
+            ),
+            nn.Sequential( # for treatment = 1
+                nn.Linear(D_in, H),
+                nn.ELU(),
+                nn.Linear(H, 1)
+            )
+
+        ])
+
+        # Freeze the BERT model
+        if freeze_bert:
+            for param in self.bert.parameters():
+                param.requires_grad = False
+        
+    def forward(self, input_ids, attention_mask):
+        """
+        Feed input to BERT and the classifier to compute logits.
+        @param    input_ids (torch.Tensor): an input tensor with shape (batch_size,
+                      max_length)
+        @param    attention_mask (torch.Tensor): a tensor that hold attention mask
+                      information with shape (batch_size, max_length)
+        @return   logits (torch.Tensor): an output tensor with shape (batch_size,
+                      num_labels)
+        """
+        # Feed input to BERT
+        outputs = self.bert(input_ids=input_ids,
+                            attention_mask=attention_mask)
+        
+        # Extract the last hidden state of the token `[CLS]` for classification task
+        last_hidden_state_cls = outputs[0][:, 0, :]
+
+        # Feed input to classifier to compute logits for propensity scores
+        logits = self.classifier(last_hidden_state_cls)
+        log_probs = torch.nn.logsoftmax(logits)
+        propensity = torch.nn.softmax(logits) # P(T=1)
+        # the loss will need logsoftmax
+
+        q_t_0 = self.expectations[0](last_hidden_state_cls) # Q(0,lambda;gamma) = E[Y|T=0,Abstract]
+        q_t_1 = self.expectations[1](last_hidden_state_cls) # Q(1,lambda;gamma) = E[Y|T=1,Abstract]
+
+        
+
+        return logits
