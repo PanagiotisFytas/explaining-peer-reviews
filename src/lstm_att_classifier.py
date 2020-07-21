6d0b088e3acc04d0ce4b9ebbebd7fe3c3bd62592
Panagiotis <pfytas@durga.shef.ac.uk>
2020-07-21 16:52:05 +0100

    reduce cuda mem

--- src/lstm_att_classifier.py
+++ src/lstm_att_classifier.py
@@ -62,9 +62,9 @@ if causal_layer == 'residual':
     epochs = 60 # 90 # 100 # 110 # 500
     batch_size = 30 # 100 # 30
     lr = 0.0001 # 0.0005
-    hidden_dimensions = [128, 64] # [128, 64] # [1500, 700, 300]
-    lstm_hidden_dimension = 4 # 30 # 300 # 500
-    num_layers = 4  # Layers in the RN. Having more than 1 layer probably makes interpretability worst by combining more tokens into hiddent embs
+    hidden_dimensions = [64] # [128, 64] # [128, 64] # [1500, 700, 300]
+    lstm_hidden_dimension = 16 # 30 # 300 # 500
+    num_layers = 1  # Layers in the RN. Having more than 1 layer probably makes interpretability worst by combining more tokens into hiddent embs
     bidirectional = False
     cell_type = 'GRU'
     causal_hidden_dimensions = [300, 164] # [64]
